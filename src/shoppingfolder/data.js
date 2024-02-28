4da91279dd93ace07d9f4bbbb795608fac257d5b
rehman github <rehmansheik519@gmail.com>
2024-02-28 19:47:34 +0530

    Shopping react ui

---
+++ src/shoppingfolder/data.js
@@ -0,0 +1,37 @@
+export const Gents={
+    title:"Gents Fashion",
+    image1:"assets/Men/1.jpeg",
+    image2:"assets/Men/2.jpeg",
+    image3:"assets/Men/3.jpeg",
+    image4:"assets/Men/4.jpeg",
+    image5:"assets/Men/5.jpeg",
+    image6:"assets/Men/6.jpeg",
+    
+    price1:"559 INR",
+    price2:"534 INR",
+    price3:"245 INR",
+    price4:"897 INR",
+    price5:"699 INR",
+    price6:"599 INR",
+   
+    }
+    
+    export const Ladies={
+    title:"Ladies Fashion",
+    image1:"assets/Women/1.jpeg",
+    image2:"assets/Women/2.jpeg",
+    image3:"assets/Women/3.jpeg",
+    image4:"assets/Women/4.jpeg",
+    image5:"assets/Women/5.jpeg",
+    image6:"assets/Women/6.jpeg",
+
+    price1:"699 INR",
+    price2:"599 INR",
+    price3:"649 INR",
+    price4:"549 INR",
+    price5:"559 INR",
+    price6:"534 INR",
+    
+    }
+    
+    
---
+++ src/shoppingfolder/pages/MainPage.jsx
@@ -0,0 +1,33 @@
+
+
+import React,{useState} from 'react'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import Collections from '../components/Collections'
+import Footer from '../components/Footer'
+
+import{Gents,Ladies} from '../data'
+import WomanCollection from '../components/WomanCollection'
+
+
+const MainPage = () => {
+
+  const [gentsFashion,setGentsFashion]=useState(Gents)
+  const [ladiesFashion,setLadiesFashion]=useState(Ladies)
+
+  console.log()
+  
+  return (
+    <div>
+        <Header />
+        <Banner />
+        <Collections gentsFashion={gentsFashion}/>
+       <WomanCollection ladiesFashion = {ladiesFashion} />
+
+        <Footer />
+       
+    </div>
+  )
+}
+
+export default MainPage
